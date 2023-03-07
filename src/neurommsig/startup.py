41b6d679fc747438da6af852bb19ee376ac928cb
zli9 <95543349+zli9@users.noreply.github.com>
2023-03-07 02:12:51 +0100

    Update and rename src/mechanrich/startup.py to src/neurommsig/startup.py

--- src/neurommsig/startup.py
+++ src/neurommsig/startup.py
@@ -6,11 +6,11 @@ from pathlib import Path
 
 # Default Directory Paths
 home_dir = Path.home()
-PROJECT_DIR = home_dir.joinpath(".mechanrich")
+PROJECT_DIR = home_dir.joinpath(".neurommsig")
 os.makedirs(PROJECT_DIR, exist_ok=True)
 
 # Logging Configuration
-LOG_FILE_PATH = os.path.join(PROJECT_DIR, "mechanrich.log")
+LOG_FILE_PATH = os.path.join(PROJECT_DIR, "neurommsig.log")
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", filename=LOG_FILE_PATH
 )
